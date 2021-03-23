fd1441af31896b139ed097dfd7fdc8a2d09f8cbb
Sridhar Ratnakumar <srid@srid.ca>
2021-03-23 14:41:08 -0400

    http client demo!

--- src/Single.Client/Main.fs
+++ src/Single.Client/Main.fs
@@ -3,35 +3,44 @@ module Single.Client.Main
 open Elmish
 open Bolero
 open Bolero.Html
-open System.IO
+open System.Net.Http
+open Microsoft.AspNetCore.Components
 
 type Model =
     {
         x: string
+        client: HttpClient
     }
-
-let initModel =
-    {
-        x = "First message"
-    }
-
 type Message =
     | SetMessage of string
+    | Loading 
 
 let update message model =
     match message with
     | SetMessage s -> { model with x = s }
+    | Loading -> { model with x = "Doing something long ..." }
+
+let handleClick model dispatch _ = 
+    dispatch Loading
+    async {
+        let! resp = model.client.GetAsync "/users/srid" |> Async.AwaitTask
+        let! s = resp.Content.ReadAsStringAsync() |> Async.AwaitTask
+        dispatch (SetMessage s)
+    } |> Async.Start
 
 let view model dispatch =
     div [] [
-        div [attr.``class`` "bg-green-500"] [
+        div [attr.``class`` "bg-green-200"] [
             text model.x
         ]
-        button [on.click (fun _ -> dispatch (SetMessage "Done!"))] [ text "Click me" ]
+        button [on.click (handleClick model dispatch)] [ text "Click me" ]
     ]
 
 type MyApp() =
     inherit ProgramComponent<Model, Message>()
 
+    [<Inject>]
+    member val GitHubClient = Unchecked.defaultof<HttpClient> with get, set
+
     override this.Program =
-        Program.mkSimple (fun _ -> initModel) update view
+        Program.mkSimple (fun _ -> { x = "Start"; client = this.GitHubClient }) update view
--- src/Single.Client/Startup.fs
+++ src/Single.Client/Startup.fs
@@ -1,14 +1,18 @@
 namespace Single.Client
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
-open Bolero.Remoting.Client
+// open Bolero.Remoting.Client
+open Microsoft.Extensions.DependencyInjection
+open System.Net.Http
+open System
 
 module Program =
-
     [<EntryPoint>]
     let Main args =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
         builder.RootComponents.Add<Main.MyApp>("#main")
-        builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
+        let apiBase = Uri("https://api.github.com")
+        builder.Services.AddSingleton<HttpClient>(new HttpClient (BaseAddress=apiBase)) |> ignore
+        // builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
         builder.Build().RunAsync() |> ignore
         0
